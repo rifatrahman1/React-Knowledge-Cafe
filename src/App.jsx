c5ad674d3b599687f84b3ee4d1bc122fed1f4dd3
rifatrahman1 <ahsan30rifat@gmail.com>
2025-01-03 19:02:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <div className='w-[1280px] mx-auto'>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="flex justify-between pt-12">
+            <h1 className='text-[40px] font-bold'>Knoledge Cafe</h1>
+            <img src="../../../knowledge-cafe/images/profile.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
